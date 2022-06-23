9c63e5e1c7c2154c47340b6db732620747a9210a
Denis <smithson11001@gmail.com>
2022-06-23 18:33:49 +0300

    Урок один 1: Небольшая правка

--- app/src/main/java/ru/gb/kotlinmoviesearch/MainActivity.kt
+++ app/src/main/java/ru/gb/kotlinmoviesearch/MainActivity.kt
@@ -14,45 +14,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         cycles()
+        val hello = Item("Breaking Bad", 2010, this@MainActivity)
 
 
         findViewById<Button>(R.id.search_button).setOnClickListener {
-            var hello = Item("Breaking Bad", 2010, this@MainActivity)
-        }
-
-        findViewById<Button>(R.id.create_item_button).setOnClickListener {
             Toast.makeText(this@MainActivity, "Work in progress", Toast.LENGTH_LONG).show()
         }
 
+        findViewById<Button>(R.id.create_item_button).setOnClickListener {
+            val copyItem = hello.copy(name = "Gladiator", year = 2004)
+        }
 
-        var hello = Item("Breaking Bad", 2010, this@MainActivity)
-        var copyItem = hello.copy()
+
+
 
 
 
     }
 
     private fun cycles() {
-        for(i in 1..10) {
-            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_LONG).show()
-            Thread.sleep(50L)
+        for(i in 1..3) {
+            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_SHORT).show()
         }
 
-        for(i in 10 downTo 1) {
-            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_LONG).show()
-            Thread.sleep(50L)
+        for(i in 3 downTo 1) {
+            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_SHORT).show()
         }
 
-        for(i in 10 downTo 1 step 2) {
-            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_LONG).show()
-            Thread.sleep(50L)
+        for(i in 4 downTo 1 step 2) {
+            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_SHORT).show()
         }
     }
 }
 
 data class Item(var name: String, var year: Int, var context: Context) {
     init {
-        Toast.makeText(context, "$name : $year created", Toast.LENGTH_LONG).show()
+        Toast.makeText(context, "$name : $year created", Toast.LENGTH_SHORT).show()
     }
 }
 
