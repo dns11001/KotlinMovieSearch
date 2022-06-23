3c0cb816af9ecaf8d9185f262e08e05e744a7bf0
Denis <smithson11001@gmail.com>
2022-06-23 18:24:30 +0300

    Урок один 1: Всё, вроде бы сделал, но скорее всего показывать сообщения тостами не всегда лучшая идея

--- app/src/main/java/ru/gb/kotlinmoviesearch/MainActivity.kt
+++ app/src/main/java/ru/gb/kotlinmoviesearch/MainActivity.kt
@@ -7,21 +7,45 @@ import android.widget.Button
 import android.widget.Toast
 import ru.gb.kotlinmoviesearch.R
 import java.security.AccessController.getContext
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cycles()
 
-        var searchButton : Button = findViewById(R.id.search_button)
-        var createButton : Button = findViewById(R.id.create_item_button)
 
-        createButton.setOnClickListener {
+        findViewById<Button>(R.id.search_button).setOnClickListener {
             var hello = Item("Breaking Bad", 2010, this@MainActivity)
         }
 
-        searchButton.setOnClickListener {
+        findViewById<Button>(R.id.create_item_button).setOnClickListener {
             Toast.makeText(this@MainActivity, "Work in progress", Toast.LENGTH_LONG).show()
+        }
+
+
+        var hello = Item("Breaking Bad", 2010, this@MainActivity)
+        var copyItem = hello.copy()
+
+
+
+    }
+
+    private fun cycles() {
+        for(i in 1..10) {
+            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_LONG).show()
+            Thread.sleep(50L)
+        }
+
+        for(i in 10 downTo 1) {
+            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_LONG).show()
+            Thread.sleep(50L)
+        }
+
+        for(i in 10 downTo 1 step 2) {
+            Toast.makeText(this@MainActivity, "$i", Toast.LENGTH_LONG).show()
+            Thread.sleep(50L)
         }
     }
 }
@@ -31,3 +55,7 @@ data class Item(var name: String, var year: Int, var context: Context) {
         Toast.makeText(context, "$name : $year created", Toast.LENGTH_LONG).show()
     }
 }
+
+
+
+
